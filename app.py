f57452df0f149f950b09126dc09b3067dbe30f7e
nobizo <138366972+nobizo@users.noreply.github.com>
2023-07-08 12:13:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
+# ユーザーインターフェイスの構築
+st.write(model + "が選ばれています。")
+st.title("CAR CHAT α 23")
+st.write("わたしはあなたのライフスタイルにあったクルマ探しのお手伝いをします。")
+
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
@@ -20,6 +25,16 @@ if "messages" not in st.session_state:
          - Information such as the manufacturer, model name, body type of the car, budget, car form, face design, safety performance, whether it's an EV or fueled, etc., are useful in narrowing down the choices.\
          - All interactions should be conducted in Japanese."}
         ]
+if model == "GPT-3.5":
+   response = openai.ChatCompletion.create(
+   model="gpt-3.5-turbo",
+   messages=messages
+   )  
+elif model == "GPT-4":
+   response = openai.ChatCompletion.create(
+   model="gpt-4",
+   messages=messages
+   )
 
 # チャットボットとやりとりする関数
 def communicate():
@@ -28,28 +43,11 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}    
     messages.append(user_message)
 
-    st.write(model + "が選ばれています。")
-
-    if model == "GPT-3.5":
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )  
-    elif model == "GPT-4":
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            engine="code-davinci-002",
-            messages=messages
-        )  
-
     bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
-# ユーザーインターフェイスの構築
-st.title("CAR CHAT α 23")
-st.write("わたしはあなたのライフスタイルにあったクルマ探しのお手伝いをします。")
 
 user_input = st.text_input("まずはあなたのニックネームと何をアドバイスしてほしいか教えてください。", key="user_input", on_change=communicate)
 
